9061a53bc5b18f1ec08135cccf3ff7a0a4a4c98e
netcdf <netcdf@example.com>
2025-10-26 23:07:04 -0400

    wip

--- flake.nix
+++ flake.nix
@@ -20,25 +20,6 @@
     in {
 
 
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
-
-
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-  
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -138,9 +119,6 @@
   # };
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 8787 80 8096 ];
